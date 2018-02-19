9f5b6f011d80220e567e36b81929a0803288f20e
sidbola <sid.s.bola@gmail.com>
2018-02-18 17:33:06 -0800

    Update config.example.js

--- config/config.example.js
+++ config/config.example.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://username:password@url:port/db',
+  db: 'mongodb://csc648-team06:iLikeMangos@url:12017/user_reports_db',
   db_dev: 'mongodb://url:port/db',
 };
