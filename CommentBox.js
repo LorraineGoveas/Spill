ad92380d39f65cab12c2f36ec0f9d577f1c15c71
Alberto Fernandez Saucedo <afernan5@mail.sfsu.edu>
2018-04-20 19:49:11 -0700

    Added state functionality, removed unused imports, added comments

--- CommentBox.js
+++ CommentBox.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import classnames from 'classnames';
 import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
-import Collapse from 'material-ui/transitions/Collapse';
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
-import Typography from 'material-ui/Typography';
 import blue from 'material-ui/colors/blue';
 import AddPhotoIcon from '@material-ui/icons/AddAPhoto';
 import PinDropIcon from '@material-ui/icons/PinDrop';
@@ -14,6 +12,10 @@ import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+
+/*
+* Inline styling for the CommentBox component
+* */
 const styles = theme => ({
     card: {
         maxWidth: 400,
@@ -46,12 +48,28 @@ const styles = theme => ({
         marginRight: theme.spacing.unit,
         width: 300,
     },
-});
+}); //end of inline styling for CommentBox
+
+// list is used for default state data
+const list = [
+    {
+        commentBoxId: 283910239238,
+        username: 'Frodo Baggins',
+        date: 'April 20, 2018',
+        avatar_image: 'F',
+        helperText: 'What would you like to post?',
+    }
+]; //end of default data
 
 class CommentBox extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
     state = {
         expanded: false,
         multiline: 'textFieldTest',
+        list: list,
     };
 
     handleExpandClick = () => {
@@ -65,11 +83,13 @@ class CommentBox extends React.Component {
         return (
 
             <div>
+                {this.state.list.map(item =>
+                    <div key={item.commentBoxId}>
                 <Card className={classes.card}>
                     <CardHeader
                         avatar={
-                            <Avatar aria-label="Recipe" className={classes.avatar}>
-                                F
+                            <Avatar aria-label="avatar" className={classes.avatar}>
+                                {item.avatar_image}
                             </Avatar>
                         }
                         action={
@@ -77,21 +97,22 @@ class CommentBox extends React.Component {
                                 <MoreVertIcon />
                             </IconButton>
                         }
-                        title="Frodo Baggins"
-                        subheader="April 22, 2018"
+                        title={item.username}
+                        subheader={item.date}
                     />
 
                     <CardContent>
                         <form className={classes.container} noValidate autoComplete="off">
                         <TextField
                             id="multiline-static"
-                            label="What would you like to post?"
+                            label={item.helperText}
                             multiline
                             rows="7"
                             defaultValue=""
                             className={classes.textField}
                             margin="normal"
-                        /></form>
+                        />
+                        </form>
                     </CardContent>
                     <CardActions className={classes.actions} disableActionSpacing>
                         <IconButton aria-label="Add to favorites">
@@ -110,6 +131,8 @@ class CommentBox extends React.Component {
 
                     </CardActions>
                 </Card>
+                    </div>
+                )}
             </div>
         );
     }
