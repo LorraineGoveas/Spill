0f690fbb66f664f5093906dcd76d44440b71164f
Satjit Bola <satjit.bola@gmail.com>
2018-05-20 22:03:09 -0700

    hotfix for linux

--- client/app/components/ReportIssue/ReportIssue.js
+++ client/app/components/ReportIssue/ReportIssue.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import style from 'styled-components';
 import Dropzone from 'react-dropzone';
-var FormData = require('form-data');
 
 const CenterPage = style.div`
     text-align: center;
